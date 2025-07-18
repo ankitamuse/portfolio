a7e8c12f26668fbab3f13e6b6f891674518d05d0
Ankita Sharma <ankitamuse@gmail.com>
2025-07-18 16:33:33 +0530

    Update config for portfolio repository and rebuild

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ankitamuse' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ankitamuse/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 }
 
 module.exports = nextConfig
